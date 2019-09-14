37df89171654d2322167b5d2f01cece220235ab8
Jonatan Eseiza <you@example.com>
2019-09-14 14:49:44 -0300

    first commit

---
+++ src/app/tab1/tab1.page.ts
@@ -0,0 +1,69 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'tab1.page.html',
+  styleUrls: ['tab1.page.scss']
+})
+export class Tab1Page {
+  public contacts = [
+    {
+      name: 'Daniela Bloque A 1B',
+      avatar: '/assets/icon/daniela.jpeg'
+    },
+    {
+      name: 'Josefa Bloque A 2B',
+      avatar: '/assets/icon/josefa.jpeg'
+    },
+    {
+      name: 'Juan Bloque B 5A',
+      avatar: '/assets/icon/juan.jpeg'
+    },
+    {
+      name: 'Liliana Bloque D 3C',
+      avatar: '/assets/icon/liliana.png'
+    },
+    {
+      name: 'Mabel Bloque D 1A',
+      avatar: '/assets/icon/mabel.jpeg'
+    },
+    {
+      name: 'Mirta Bloque A 2C',
+      avatar: '/assets/icon/mirta.jpeg'
+    },
+    {
+      name: 'Pedro Bloque B 1C',
+      avatar: '/assets/icon/pedro.jpeg'
+    },
+    {
+      name: 'Sebastian Bloque D 4F',
+      avatar: '/assets/icon/sebastian.jpeg'
+    },
+    {
+      name: 'Roberto Bloque B 4B',
+      avatar: '/assets/icon/tito.jpg'
+    },
+    {
+      name: 'Raul Bloque A 5C',
+      avatar: '/assets/icon/raul.jpeg'
+    },
+    {
+      name: 'Josefa Bloque A 2B',
+      avatar: '/assets/icon/josefa.jpeg'
+    },
+    {
+      name: 'Juan Bloque B 5A',
+      avatar: '/assets/icon/juan.jpeg'
+    },
+    {
+      name: 'Daniela Bloque A 1B',
+      avatar: '/assets/icon/daniela.jpeg'
+    },
+    {
+      name: 'Josefa Bloque A 2B',
+      avatar: '/assets/icon/josefa.jpeg'
+    }
+  ];
+  constructor() {}
+
+}
